15f80d407c14532de9e21aeab715749a8e36cbf6
RemziBalakishiyev <remzibalakishiyev13@gmail.com>
2022-09-06 22:56:05 +0400

    Desig located

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,15 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { SharedModule } from './Shared/shared.module';
 import { AccountModule } from './modules/account/account.module';
+import { SidebarComponent } from './modules/Sidebar/feature/sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  
-  ],
-  imports: [
-    SharedModule,
-    AccountModule
-  ],
+  declarations: [AppComponent, SidebarComponent],
+  imports: [SharedModule, AccountModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
